87ef058b509bb90129e64f6f6082ac0e58be09bf
Rishav <rishavj032@gmail.com>
2023-08-04 18:58:18 +0530

    React JSX-001

---
+++ React-Jsx-001/Readme.md
@@ -0,0 +1,4 @@
+The Project is to make a simple page using jsx
+     I have use a heading tag for giving a heading
+     and i have use a paragraph tag for the paragraph
+     
---
+++ React-Jsx-001/script.js
@@ -0,0 +1,28 @@
+function Main (){
+    return(
+        <div class = "main">
+            <div class = "head">
+                <h1>
+                    About Us
+                </h1>
+            </div>
+            <div class = "para1">
+                <p>
+                   At Zappos.com,our purpose is simple: to live and deliver WOW. 
+                </p>
+            </div>
+            <div class = "para2">
+            &nbsp;&nbsp;Twenty years ago we began as a small online retailer that only sold shoes. Today we still sell shoes --- as well as <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;clothing handbags accessories, and more . That "more" is providing the very best customer service ,customer<br/>
+                  &nbsp;&nbsp;experience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver<br/>
+                  &nbsp;&nbsp;happiness to customers, employees, vendors shareholders,and the communities in a long-term, sustainable way.<br/>
+                  <br/>
+                  <br />
+                  We hope that in the future people won't even realise we started selling shoes,online,instead they'll know Zappos as a <br/>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;service company that just happens to sell ___________.
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Main />,document.getElementById('root'))
