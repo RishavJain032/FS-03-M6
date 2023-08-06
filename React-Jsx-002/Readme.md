f4fdf1abcff90475eb05a77b5ebdd992d55e748f
Rishav <rishavj032@gmail.com>
2023-08-06 19:43:10 +0530

    React JSX-002

---
+++ React-Jsx-002/Readme.md
@@ -0,0 +1,2 @@
+In these assignment i have use h1,h2 tag for giving heading 
+and i have use a image also that i have taken from internet.
---
+++ React-Jsx-002/script.js
@@ -0,0 +1,28 @@
+function Main (){
+    return(
+        <div class = "main">
+            <div>
+                <h1 class = "head1">
+                    &nbsp;&nbsp;&nbsp;The Evolution of <br/>
+                    Airbnb's Frontend
+                </h1>
+
+
+                <div>
+                    <h2 class = "head">
+                        @spikebrehm
+                    </h2>
+                </div>
+
+                <div class = "air">
+                <img src = "./airbnb-logo.png" width = "140px" height = "100px"/>
+                
+                </div>
+    
+            </div>
+            
+        </div>
+    )
+}
+
+ReactDOM.render(<Main />,document.getElementById('root'))
